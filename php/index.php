89555180acbde27ea7524a0531a7e4cd61365334
ppkpub <ppkpub@gmail.com>
2017-08-06 10:39:22 +0800

    Update for distributed function demo

--- php/index.php
+++ php/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  PPk AP Sample V0.1
+  PPk AP Sample
 */
 define(PPK_URI_PREFIX,"ppk:");
 define(TEST_CODE_UTF8,"A测B试C");
@@ -8,12 +8,14 @@ define(TEST_CODE_UTF8,"A测B试C");
 define(AP_RESOURCE_PATH, getcwd()."/resource/" ); //资源内容存放路径
 define(AP_KEY_PATH,      getcwd()."/key/" );      //密钥文件存储路径
 
+//$FRIEND_AP_LIST_FROM_BLOCKCHAIN_FROM_BLOCKCHAIN=array('http://ppk001.sinaapp.com/ap/');
+
 //从header中提取请求数据
 //  ppk-uri :  ppk:385617.1822/
 //或者在URL中指定 ?ppk-uri=ppk:385617.1822/
 $ppk_reqs=array();
 
-if($_GET['ppk-uri']!=null){ 
+if($_GET['ppk-uri']!=null){  //Just for test
   $ppk_reqs['ppk-uri']=$_GET['ppk-uri'];
 }else{
   if (!function_exists('getallheaders')) { 
@@ -44,12 +46,12 @@ if( 0!=strncasecmp($str_req_ppk_uri,PPK_URI_PREFIX,strlen(PPK_URI_PREFIX)) ){
 $odin_chunks=array();
 $parent_odin_path="";
 $resource_id="";
-$resource_versoin="";
+$req_resource_versoin="";
 $resource_filename="";
 
 $tmp_chunks=explode("#",substr($str_req_ppk_uri,strlen(PPK_URI_PREFIX)));
 if(count($tmp_chunks)>=2){
-  $resource_versoin=$tmp_chunks[1];
+  $req_resource_versoin=$tmp_chunks[1];
 }
 
 $odin_chunks=explode("/",$tmp_chunks[0]);
@@ -63,57 +65,117 @@ if(count($odin_chunks)==1){
 }
 
 //echo "str_req_ppk_uri=$str_req_ppk_uri\n";
-//echo "parent_odin_path=$parent_odin_path , resource_id=$resource_id , resource_versoin=$resource_versoin \n";
+//echo "parent_odin_path=$parent_odin_path , resource_id=$resource_id , req_resource_versoin=$req_resource_versoin \n";
 
 if(strlen($parent_odin_path)==0){
   echo "The root ODIN should be parsed from bitcoin blockchain directly or from the cetified api service!";
   exit(-1);
 }
 
-//从资源目录下定位最新版本
-if(!file_exists( AP_RESOURCE_PATH.$parent_odin_path )){
-  //header("HTTP/1.1 404 Not Found");
-  echo "resource_dir:$parent_odin_path  not exist";
-  exit(-1);
-}
+$tmp_posn1=strpos($resource_id,'?');
+$tmp_posn2=strpos($resource_id,'(');
+if($tmp_posn1!==false||$tmp_posn2!==false){
+  //如果resource_id的第一个字符为?或(，按传入参数方式动态处理
+  $function_name='';
+  if($tmp_posn1!==false){
+    $function_name=substr($resource_id,0,$tmp_posn1);
+    $argvs_chunks=explode("&",substr($resource_id,$tmp_posn1+1));
+  }else  if($tmp_posn2!==false){
+    $function_name=substr($resource_id,0,$tmp_posn2);
+    $argvs_chunks=explode(",",substr($resource_id,$tmp_posn2+1,strlen($resource_id)-$tmp_posn2-2));
+  }
+  //print_r($argvs_chunks);
+  $tmp_resp=array();
+  $tmp_resp['function_name']=$function_name;
+  $tmp_resp['function_argvs']=$argvs_chunks;
+  
+  if($function_name=='sum'){
+    $tmp_result=0;
+    foreach($argvs_chunks as $tmp_value){
+      $tmp_result += $tmp_value;
+    }
+    $tmp_resp['function_result']=$tmp_result;
+  }else{
+    $tmp_resp['function_result']="OK";
+  }
+  
+  $tmp_resp['time']=strftime("20%y-%m-%d %H:%M:%S",time());
+  $str_resp_content=json_encode($tmp_resp);
+  $resp_resource_versoin=strftime("20%y%m%d%H%M%S",time()).'.0';//示例代码简单以时间值作为动态版本编号
+  
+  $str_resp_uri=PPK_URI_PREFIX.$parent_odin_path.$resource_id."#".$resp_resource_versoin;
+}else{
+  //从静态资源目录下定位最新内容数据版本
+  if(!file_exists( AP_RESOURCE_PATH.$parent_odin_path )){
+    //header("HTTP/1.1 404 Not Found");
+    //尝试从其他关联AP节点对等获取
+    for($kk=0;$kk<count($FRIEND_AP_LIST_FROM_BLOCKCHAIN);$kk++){
+      $tmp_friend_ap_uri=$FRIEND_AP_LIST_FROM_BLOCKCHAIN[$kk].'?ppk-uri='.url_encode($str_req_ppk_uri);
+      echo 'tmp_friend_ap_uri=',$tmp_friend_ap_uri,"<br>\n";
+      
+      //$ppk_sign=
+      
+      if(strlen($ppk_sign)>0){
+        setcookie("ppk-sign",$ppk_sign);
+        $resp_text=file_get_contents($tmp_friend_ap_uri);
 
-$d = dir(AP_RESOURCE_PATH.$parent_odin_path);
-while (($filename = $d->read()) !== false){
-  //echo "filename: " . $filename . "<br>";
-  $tmp_chunks=explode("#",$filename);
-  $req_resource_versoin=$resource_versoin;
-  if( strcmp($tmp_chunks[0],$resource_id)==0 && count($tmp_chunks)>=3){
-    if(strlen($req_resource_versoin)==0){
-      if(strlen($resource_versoin)==0){
-        $resource_versoin = $tmp_chunks[1];
-        $resource_filename = $filename;
-      }else if( cmpResourceVersion($tmp_chunks[1],$resource_versoin)>0  ){ 
-        $resource_versoin = $tmp_chunks[1];
+        header('Content-Type: text/html');
+        echo $resp_text;
+        exit(-1);
+      }
+    }
+    
+    echo "resource_dir:$parent_odin_path  not exist";
+    exit(-1);
+  }
+
+  $d = dir(AP_RESOURCE_PATH.$parent_odin_path);
+  while (($filename = $d->read()) !== false){
+    //echo "filename: " . $filename . "<br>";
+    $tmp_chunks=explode("#",$filename);
+    
+    if( strcmp($tmp_chunks[0],$resource_id)==0 && count($tmp_chunks)>=3){
+      //print_r($tmp_chunks);
+      if(strlen($req_resource_versoin)==0){
+        if(strlen($resp_resource_versoin)==0){
+          $resp_resource_versoin = $tmp_chunks[1];
+          $resource_filename = $filename;
+        }else if( cmpResourceVersion($tmp_chunks[1],$resp_resource_versoin)>0  ){ 
+          $resp_resource_versoin = $tmp_chunks[1];
+          $resource_filename = $filename;
+        }
+      }else if( strcmp($tmp_chunks[1],$req_resource_versoin)==0 ){
+        $resp_resource_versoin=$req_resource_versoin;
         $resource_filename = $filename;
       }
-    }else if( strcmp($tmp_chunks[1],$req_resource_versoin)==0 ){
-      $resource_filename = $filename;
     }
   }
+  $d->close();
+
+  if(strlen($resp_resource_versoin)==0)
+    $resp_resource_versoin = '1.0'; //Default resource version
+
+  $str_resp_uri=PPK_URI_PREFIX.$parent_odin_path.$resource_id."#".$resp_resource_versoin;
+
+  $resource_path_and_filename = AP_RESOURCE_PATH.$parent_odin_path.$resource_filename;
+  //echo "resource_path_and_filename=$resource_path_and_filename , resource_id=$resource_id , resp_resource_versoin=$resp_resource_versoin \n";
+
+  if(!file_exists($resource_path_and_filename )){
+    header("HTTP/1.1 404 Not Found");
+    //echo "$resource_path_and_filename not exist";
+    exit(-1);
+  }
+
+  //读取资源内容文件
+  $str_resp_content=@file_get_contents($resource_path_and_filename);
+  
+  $ext = pathinfo($resource_path_and_filename, PATHINFO_EXTENSION);
 }
-$d->close();
 
-if(strlen($resource_versoin)==0)
-  $resource_versoin = '1.0';
-
-$str_resp_uri=PPK_URI_PREFIX.$parent_odin_path.$resource_id."#".$resource_versoin;
-
-$resource_path_and_filename = AP_RESOURCE_PATH.$parent_odin_path.$resource_filename;
-//echo "resource_path_and_filename=$resource_path_and_filename , resource_id=$resource_id , resource_versoin=$resource_versoin \n";
-
-if(!file_exists($resource_path_and_filename )){
-  header("HTTP/1.1 404 Not Found");
-  //echo "$resource_path_and_filename not exist";
-  exit(-1);
-}
-
-//读取资源内容文件
-$str_resp_content=@file_get_contents($resource_path_and_filename);
+//test
+//$str_resp_uri='ppk:426137.1518/uap2/sensor_b#1.0';
+//$str_resp_content='1';
+//end test
 
 //生成签名
 $ppk_sign=generatePPkSign($str_resp_uri,$str_resp_content);
@@ -123,7 +185,6 @@ $ppk_sign=generatePPkSign($str_resp_uri,$str_resp_content);
 setcookie("ppk-sign",$ppk_sign);
 
 //输出内容类型头定义
-$ext = pathinfo($resource_path_and_filename, PATHINFO_EXTENSION);
 if( strcasecmp($ext,'jpeg')==0 || strcasecmp($ext,'jpg')==0 || strcasecmp($ext,'gif')==0 || strcasecmp($ext,'png')==0)
   header('Content-Type: image/'.$ext);
 else  
@@ -170,8 +231,8 @@ function generatePPkSign($str_resp_uri,$str_resp_content){
 }
 
 //获取请求资源对应的签名验证设置
-function getOdinKeySet($parent_odin_path,$resource_id,$resource_versoin=''){
-  //echo "getOdinKeySet(): parent_odin_path=$parent_odin_path,resource_id=$resource_id,resource_versoin=$resource_versoin\n";
+function getOdinKeySet($parent_odin_path,$resource_id,$resp_resource_versoin=''){
+  //echo "getOdinKeySet(): parent_odin_path=$parent_odin_path,resource_id=$resource_id,resp_resource_versoin=$resp_resource_versoin\n";
   
   $key_filename=AP_KEY_PATH.$parent_odin_path.'/'.$resource_id.'.json';
   
@@ -209,14 +270,17 @@ function rsaSign($data,$strValidationPrvkey,$algo){
 
 
 /*
-比较资源版本
+比较资源版本号
 */
 function cmpResourceVersion($rv1,$rv2){
+  //echo $rv1,'  vs ',$rv2,"<br>";
   $tmp_chunks=explode(".",$rv1);
+  //print_r($tmp_chunks);
   $major1 =  intval($tmp_chunks[0]);
   $minor1 =  count($tmp_chunks)>1 ? intval($tmp_chunks[1]):0;
   
   $tmp_chunks=explode(".",$rv2);
+  //print_r($tmp_chunks);
   $major2 =  intval($tmp_chunks[0]);
   $minor2 =  count($tmp_chunks)>1 ? intval($tmp_chunks[1]):0;
   
@@ -265,3 +329,14 @@ function strToHex($data, $newline="n")
   return $str_hex;
 }  
 
+
+
+/*
+//从数据库提取ODIN标识
+$dbh = new PDO('sqlite:/oddi.db'); 
+foreach ($dbh->query('SELECT * FROM odiis') as $row) // prints invalid resource
+{
+    echo $row['full_odii'],"",$row['owner'],"<br>\n";
+    
+}
+*/
